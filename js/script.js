c305861d5bc8f4c30ec9d3bd89eb90c5bf5d78d2
JereLeao <jeremaios@gmail.com>
2020-11-27 15:18:03 -0300

    changes in the code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const globalNames = ['teste1', 'teste2'];
 /**
- * Fazer o download da API de acordo com a URL
+ * Fazer o download da API de acordo com a URL e guardar em um array
+ * filtrar com base no input, ativado pelo botao ou 'ENTER'
+ * if tiver mais de um caracter
+ * montar dois paineis
+ * um com usuarios filtrados
+ * segudno com estatisticas
  */
 
 
@@ -24,10 +29,26 @@ const preventFormSubmit = () => {
  * function to get the input from html
  */
 const getInput = () => document.querySelector('#inputName');
+const getButton = () => document.getElementById('searchButton')
 
 const activateInput = () => {
   let inputName = getInput();
-  inputName.addEventListener('keyup', () => {
-    
+  let searchButton = getButton();
+  inputName.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      return handleSearch
+    }
   });
+  searchButton.addEventListener('click', handleSearch);
+
+  function handleSearch() {
+    console.log('enter pressed');
+  }
+  clear();
+}
+function clear() {
+  var inputName = getInput();
+  inputName.value = '';
+  inputName.focus();
+  globalIsEditing = false;
 }
